0880ac77513c2d0bd17adf58e5faa01c3ed1f15c
Leila Elshamy <leila.m.elshamy@gmail.com>
2018-12-11 00:43:54 -0800

    added pointer functionality to tester

--- src/pages/Tester.js
+++ src/pages/Tester.js
@@ -9,8 +9,23 @@ class Tester extends Component {
         lat: 32.7096422,    //default downtown San Diego
         lng: -117.1579546
       },
-      zoom: 14,
-      address: ''
+      zoom: 11,
+      address: '',
+      coordinatesArr: [
+        {
+          lat: 32.8328112,
+          lng: -117.2712717
+        },
+        {
+          lat: 32.6400541,
+          lng: -117.0841955
+        },
+        {
+          lat: 32.7565162,
+          lng: -117.1122965
+        }
+      ],
+      newCoordinates: {}
     }
   }
 
@@ -22,16 +37,15 @@ class Tester extends Component {
 
   handleSearch = (e) => {
     const url = 'https://maps.googleapis.com/maps/api/geocode/json'
-    const apiKey = 'APIKEY'
+    const apiKey = 'API_KEY'
     e.preventDefault()
-    console.log(this.state.address)
     fetch(url + '?address=' + this.state.address + '&key=' + apiKey)
     .then(resp => {
       let json = resp.json()
       return json
     })
     .then(json => {
-      console.log(json.results[0].geometry.location) // <-- to get obj containing lat/lng
+      console.log('json response::', json.results[0].geometry.location) // <-- to get obj containing lat/lng
       this.setState({
         center: {
           lat: json.results[0].geometry.location.lat,
@@ -44,20 +58,74 @@ class Tester extends Component {
     })
   }
 
+  handleAddPointer = (e) => {
+    let { coordinatesArr } = this.state
+    const url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    const apiKey = 'API_KEY'
+    e.preventDefault()
+    fetch(url + '?address=' + this.state.address + '&key=' + apiKey)
+    .then(resp => {
+      let json = resp.json()
+      return json
+    })
+    .then(json => {
+      console.log('json response::', json.results[0].geometry.location) // <-- to get obj containing lat/lng
+      coordinatesArr.push({
+        lat: json.results[0].geometry.location.lat,
+        lng: json.results[0].geometry.location.lng
+      })
+      this.setState({
+        coordinatesArr: coordinatesArr
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+
+  }
+
+  determinePointer = () => {
+    const pointerStyle = {
+      position: 'absolute',
+      width: 40,
+      height: 40,
+      left: -40 / 2,
+      top: -40 / 2,
+      border: '3px solid green',
+      borderRadius: 40,
+      backgroundColor: 'white',
+      opacity: 0.6,
+      textAlign: 'center',
+      color: '#3f51b5',
+      fontSize: 16,
+      fontWeight: 'bold',
+      padding: 4
+    }
+    return this.state.coordinatesArr.map(coordinates => {
+      return (
+        <div style={pointerStyle} lat={coordinates.lat} lng={coordinates.lng}>
+        </div>
+      )
+    })
+  }
+
   render() {
     return (
       <div className="page">
         <div style={{ height: '75vh', width: '70%', margin: '0 auto' }}>
           <GoogleMapReact
-            bootstrapURLKeys={{ key: 'AIzaSyCYw5lzQYX9SL8qlqqKZ5latLcBuRYfDyQ' }}
+            bootstrapURLKeys={{ key: 'API_KEY' }}
             center={this.state.center}
-            defaultZoom={this.state.zoom}
-          />
+            zoom={this.state.zoom}>
+            {this.determinePointer()}
+
+          </GoogleMapReact>
         </div>
         <br/>
         <form>
           <input type="text" name="address" placeholder="Enter Address" onChange={this.handleChange} />
           <input type="submit" value="Submit" onClick={this.handleSearch} />
+          <input type="submit" value="Add Pointer" onClick={this.handleAddPointer} />
         </form>
       </div>
     );
