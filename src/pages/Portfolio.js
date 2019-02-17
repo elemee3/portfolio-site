46eb3fa6bfc43f4fcbe9172a95e866c0abc5fd65
Leila Elshamy <leila.m.elshamy@gmail.com>
2019-02-16 23:24:03 -0800

    minor restructuring

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -17,10 +17,8 @@ class Portfolio extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      activeTab: '1',
       modalIsOpen: false
     }
-    this.toggle = this.toggle.bind(this);
   }
 
   onOpenModal = () => {
@@ -31,147 +29,69 @@ class Portfolio extends Component {
     this.setState({ modalIsOpen: false });
   }
 
-  toggle(tab) {
-    if (this.state.activeTab !== tab) {
-      this.setState({
-        activeTab: tab
-      });
-    }
-  }
   render() {
     return (
       <div className="page">
         <h3>Portfolio</h3>
-        <Nav tabs>
+        <div className="portfolio-tab">
 
-          <NavItem>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === '1' })}
-              onClick={() => { this.toggle('1') }}
-            >
-              Currently Deployed
-            </NavLink>
-          </NavItem>
+            <Card body className="single-card">
+              <CardTitle>Cat Tinder</CardTitle>
+              <CardSubtitle>ReactJS frontend, Ruby on Rails backend</CardSubtitle>
+              <br/>
+              <CardText>Cat Tinder is currently a database of cats that can be viewed, added, or deleted by users. Going forward, my goal is to implement a voting mechanism where users can up/down vote cats.</CardText>
+              <Button className="portfolio-button" href="https://protected-citadel-24964.herokuapp.com/">Launch (Heroku)</Button>
+            </Card>
 
-          <NavItem>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === '2' })}
-              onClick={() => { this.toggle('2') }}
-            >
-              Full-Stack Apps
-            </NavLink>
-          </NavItem>
+            <Card body className="single-card">
+              <CardTitle>Activity Buddy</CardTitle>
+              <CardSubtitle>ReactJS frontend, Ruby on Rails backend</CardSubtitle>
+              <br/>
+              <CardText>Activity Buddy is a full-stack app that includes authetication/authorization via Devise. It allows a user to register with their address and activity preference, and locate other users near them.</CardText>
+              <Button className="portfolio-button" href="http://activitybuddy-frontend.herokuapp.com/">Launch (Heroku)</Button>
+            </Card>
 
-          <NavItem>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === '3' })}
-              onClick={() => { this.toggle('3') }}
-            >
-              ReactJS
-            </NavLink>
-          </NavItem>
+            <Card body className="single-card">
+              <CardTitle>Tic Tac Toe</CardTitle>
+              <CardSubtitle>ReactJS Mini-Game</CardSubtitle>
+              <br/>
+              <CardText>A simple game of tic-tac-toe built from scratch in ReactJS. Players alternate turns, beginning with player 'X'. The game is over when a player gets 3 in a row, or no empty squares are left. Try 'Legendary Mode' for an additional challenge!</CardText>
+              <Button className="portfolio-button" onClick={this.onOpenModal}>Open</Button>
+              <ReactModal
+                style={modalStyle}
+                open={this.state.modalIsOpen}
+                onClose={this.onCloseModal}
+              >
+                <TicTacToe />
+              </ReactModal>
 
-          <NavItem>
-            <NavLink
-              className={classnames({ active: this.state.activeTab === '4' })}
-              onClick={() => { this.toggle('4') }}
-            >
-              Ruby on Rails
-            </NavLink>
-          </NavItem>
+            </Card>
 
-        </Nav>
+            <Card body className="single-card">
+              <CardTitle>Pig Latin</CardTitle>
+              <CardSubtitle>ReactJS Mini-Game</CardSubtitle>
+              <br/>
+              <CardText>The Pig Latin app allows users to submit plain English and get back a translation to Pig Latin.</CardText>
+              <Button className="portfolio-button" href="https://pig-latify1.herokuapp.com/">Launch (Heroku)</Button>
+            </Card>
 
+            <Card body className="single-card">
+              <CardTitle>Battleship</CardTitle>
+              <CardSubtitle>ReactJS Mini-Game</CardSubtitle>
+              <br/>
+              <CardText>Work In Progress</CardText>
+              <Button className="portfolio-button" href="#">Coming Soon!</Button>
+            </Card>
 
-        <TabContent activeTab={this.state.activeTab}>
+            <Card body className="single-card">
+              <CardTitle>YummY</CardTitle>
+              <CardSubtitle>Ruby on Rails</CardSubtitle>
+              <br/>
+              <CardText>Work In Progress</CardText>
+              <Button className="portfolio-button" href="#">Coming Soon!</Button>
+            </Card>
 
-          <TabPane tabId="1">
-            <div className="portfolio-tab">
-
-                <Card body className="single-card">
-                  <CardTitle>Cat Tinder</CardTitle>
-                  <CardSubtitle>ReactJS frontend, Ruby on Rails backend</CardSubtitle>
-                  <br/>
-                  <CardText>Cat Tinder is currently a database of cats that can be viewed, added, or deleted by users. Going forward, my goal is to implement a voting mechanism where users can up/down vote cats.</CardText>
-                  <Button className="portfolio-button" href="https://protected-citadel-24964.herokuapp.com/">Launch</Button>
-                </Card>
-
-                <Card body className="single-card">
-                  <CardTitle>Activity Buddy</CardTitle>
-                  <CardSubtitle>ReactJS frontend, Ruby on Rails backend</CardSubtitle>
-                  <br/>
-                  <CardText>Activity Buddy is a full-stack app that includes authetication/authorization via Devise. It allows a user to register with their address and activity preference, and locate other users near them.</CardText>
-                  <Button className="portfolio-button" href="http://activitybuddy-frontend.herokuapp.com/">Launch</Button>
-                </Card>
-
-                <Card body className="single-card">
-                  <CardTitle>YummY</CardTitle>
-                  <CardSubtitle>Ruby on Rails</CardSubtitle>
-                  <br/>
-                  <CardText>Coming Soon!</CardText>
-                  <Button className="portfolio-button" href="#">Coming Soon!</Button>
-                </Card>
-
-                <Card body className="single-card">
-                  <CardTitle>Pig Latin</CardTitle>
-                  <CardSubtitle>ReactJS Mini-Game</CardSubtitle>
-                  <br/>
-                  <CardText>The Pig Latin app allows users to submit plain English and get back a translation to Pig Latin.</CardText>
-                  <Button className="portfolio-button" href="https://pig-latify1.herokuapp.com/">Launch</Button>
-                </Card>
-
-                <Card body className="single-card">
-                  <CardTitle>Battleship</CardTitle>
-                  <CardSubtitle>ReactJS Mini-Game</CardSubtitle>
-                  <br/>
-                  <CardText>Coming Soon!</CardText>
-                  <Button className="portfolio-button" href="#">Coming Soon!</Button>
-                </Card>
-
-                <Card body className="single-card">
-                  <CardTitle>Tic Tac Toe</CardTitle>
-                  <CardSubtitle>ReactJS Mini-Game</CardSubtitle>
-                  <br/>
-                  <CardText>A simple game of tic-tac-toe built from scratch in ReactJS. Players alternate turns, beginning with player 'X'. The game is over when a player gets 3 in a row, or no empty squares are left. Try 'Legendary Mode' for an additional challenge!</CardText>
-                  <Button className="portfolio-button" onClick={this.onOpenModal}>Launch</Button>
-                  <ReactModal
-                    style={modalStyle}
-                    open={this.state.modalIsOpen}
-                    onClose={this.onCloseModal}
-                  >
-                    <TicTacToe />
-                  </ReactModal>
-
-                </Card>
-              </div>
-
-          </TabPane>
-
-          <TabPane tabId="2">
-            <Row>
-              <Col sm="12">
-                <h4>Coming Soon!</h4>
-              </Col>
-            </Row>
-          </TabPane>
-
-          <TabPane tabId="3">
-            <Row>
-              <Col sm="12">
-                <h4>Coming Soon!</h4>
-              </Col>
-            </Row>
-          </TabPane>
-
-          <TabPane tabId="4">
-            <Row>
-              <Col sm="12">
-                <h4>Coming Soon!</h4>
-              </Col>
-            </Row>
-          </TabPane>
-
-        </TabContent>
+          </div>
       </div>
     );
   }
